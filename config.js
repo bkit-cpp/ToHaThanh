621aa288e88613eadfe81d902721b1536dbdb8d4
LBK <bakhaipth@gmail.com>
2023-10-20 01:19:13 +0700

    Upload
    Upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Hà Thanhhhhhhh Iuuuuuu';
+const giftUrl = 'index2.html';
+const eventName = 'Chúc Mừng 20/10';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc Hà Thanh xinh đẹp dễ thương yêu dấu tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cậu trong hôm nay và cả những ngày sau';
+
+const giftImage = 'HT1.png';
+const base64 = '';
+const giftImageBase64 = "HT2.png " + base64;
